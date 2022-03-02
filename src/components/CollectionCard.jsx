29bbcfd428324c1b706c4233ddeacd04a987d1e8
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-03-02 17:33:09 +0100

    details

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -6,18 +6,19 @@ const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
       <img src={image} alt={name} />
-      <div className="details"></div>
-      <div className="name">
-        {name} <div className="id">.#{id}</div>
-      </div>
-      {/* ------ */}
-      <div className="priceContainer">
-        <img src={wethIcon} alt={wethIcon} className="wethImg" />
-        {/* the traits is data coming from opensea, with the
+      <div className="details">
+        <div className="name">
+          {name} <div className="id">.#{id}</div>
+        </div>
+        {/* ------ */}
+        <div className="priceContainer">
+          <img src={wethIcon} alt={wethIcon} className="wethImg" />
+          {/* the traits is data coming from opensea, with the
         traits we will have access to all the traits or 
         properties from the "the minted art 
         we did in the beginning" */}
-        <div className="price">{traits[0]?.value}</div>
+          <div className="price">{traits[0]?.value}</div>
+        </div>{" "}
       </div>
     </div>
   );
